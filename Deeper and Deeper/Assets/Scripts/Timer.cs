9fa45e40d1050bf9efbb2533943911a2b6034c96
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2021-04-24 19:06:22 -0700

    Adding Timer Display

--- Deeper and Deeper/Assets/Scripts/Timer.cs
+++ Deeper and Deeper/Assets/Scripts/Timer.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public float timeRemaining = 10;
+    [SerializeField] float minutes = 10;
+    [SerializeField] float seconds = 0;
+    [SerializeField] Text displayTimer;
+    private float remainingTime ;
+    private bool timerActive = false;
+
+    void Start() {
+        remainingTime = minutes * 60 + seconds;
+        timerActive = true;
+    }
 
     void Update()
     {
-        if (timeRemaining > 0)
+       if (timerActive)
         {
-            timeRemaining -= Time.deltaTime;
+             if (remainingTime > 0)
+            {
+                remainingTime -= Time.deltaTime;
+            }
+            
+            else
+            {
+                timerActive = false;
+                remainingTime = 0;
+                Debug.Log("Time has run out!");
+            }
+
+                DisplayTime(remainingTime);
         }
+    }
+
+    void DisplayTime(float remainingTimeDisplayed)
+    {
+        remainingTimeDisplayed += 1;
+        
+        float minutes = Mathf.FloorToInt(remainingTimeDisplayed / 60); 
+        float seconds = Mathf.FloorToInt(remainingTimeDisplayed % 60);
+
+        displayTimer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
