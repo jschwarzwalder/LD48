0738dcaa438ef7d405ae2a45ecb3585d4f292237
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2021-04-26 02:22:42 -0700

    Testing out Different camera Movement

--- Deeper and Deeper/Assets/Scripts/CameraMovement.cs
+++ Deeper and Deeper/Assets/Scripts/CameraMovement.cs
@@ -1,50 +1,71 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
-public class CameraMovement : MonoBehaviour {
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] Transform playerCamera = null;
+    [SerializeField] float mouseSensitivity = 3.5f;
+    [SerializeField] float walkSpeed = 6.0f;
+    [SerializeField] float gravity = -13.0f;
+    [SerializeField][Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
+    [SerializeField][Range(0.0f, 0.5f)] float mouseSmoothTime = 0.03f;
 
-    [SerializeField] float moveSpeed = 0.5f;
-    [SerializeField] float scrollSpeed = 10f;
-    [SerializeField] float maxX;
-    [SerializeField] float minX;
-    [SerializeField] float maxZ;
-    [SerializeField] float minZ;
-    bool withinRoom;
+    [SerializeField] bool lockCursor = true;
 
-    void Update () {
+    float cameraPitch = 0.0f;
+    float velocityY = 0.0f;
+    CharacterController controller = null;
 
-        withinRoom = transform.position.x > minX && transform.position.x < maxX && 
-                     transform.position.z > minZ && transform.position.z < maxZ;
+    Vector2 currentDir = Vector2.zero;
+    Vector2 currentDirVelocity = Vector2.zero;
 
-        Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    Vector2 currentMouseDelta = Vector2.zero;
+    Vector2 currentMouseDeltaVelocity = Vector2.zero;
 
-        if (withinRoom){
-
-            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
-                transform.position += moveSpeed * new Vector3(Input.GetAxisRaw("Vertical"), 0, Input.GetAxisRaw("Horizontal"));
-            }
-
-            if (Input.GetAxis("Mouse ScrollWheel") != 0) {
-                transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
-            } 
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
-         else {
-              if (transform.position.x < minX) {
-                  position.x = minX + .1f;
-              } 
-              if (transform.position.x > maxX) {
-                  position.x = maxX - .1f;
-              }
-               if (transform.position.z < minZ) {
-                  position.z = minZ + .1f;
-              } 
-              if (transform.position.z > maxZ) {
-                  position.z = maxZ - .1f;
-              }
-
-              transform.position = position;
-         }
     }
 
+    void Update()
+    {
+        UpdateMouseLook();
+        UpdateMovement();
+    }
+
+    void UpdateMouseLook()
+    {
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
+
+        cameraPitch -= currentMouseDelta.y * mouseSensitivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
+
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+        transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
+    }
+
+    void UpdateMovement()
+    {
+        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        targetDir.Normalize();
+
+        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
+
+        if(controller.isGrounded)
+            velocityY = 0.0f;
+
+        velocityY += gravity * Time.deltaTime;
+		
+        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * walkSpeed + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
 }
