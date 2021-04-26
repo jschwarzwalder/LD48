5dfe0927242be899fd10cc7f331a5f5cdb328a85
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2021-04-25 20:10:51 -0700

    Adding Volume Control

---
+++ Deeper and Deeper/Assets/Scripts/SetVolume.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour {
+
+     [SerializeField] AudioMixer mixer;
+     [SerializeField] string mixerReferenceName;
+
+    public void SetLevel (float sliderValue)
+    {
+        mixer.SetFloat(mixerReferenceName, Mathf.Log10(sliderValue) * 20);
+    }
+}
